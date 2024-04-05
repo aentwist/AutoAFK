4ca4c1f6ae8f6de536e6523f004600987b5df55e
Hammanek <hammanecek@gmail.com>
2024-04-06 01:42:18 +0200

    version bump

--- AutoAFK.py
+++ AutoAFK.py
@@ -34,7 +34,7 @@ else:
     settings = os.path.join(cwd, 'settings.ini')
 config.read(settings)
 
-version = "0.16.6"
+version = "0.17"
 
 repo_releases = requests.get('https://api.github.com/repos/Hammanek/AutoAFK/releases/latest')
 json = repo_releases.json() if repo_releases and repo_releases.status_code == 200 else None
