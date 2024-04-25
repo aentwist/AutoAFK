7b31ab246d898912d4e507b53ff240e5256de58d
Hammanek <hammanecek@gmail.com>
2024-04-25 19:21:22 +0200

    delete

--- minipc.bat
+++
@@ -1,7 +0,0 @@
-py -3.9 AutoAFK.py --dailies
-py -3.9 ../../AutoAFKJourney/AutoAFKJourney/AutoAFK2.py --teamup
-taskkill /F /IM adb.exe
-taskkill /F /IM MuMuPlayer.exe
-taskkill /F /IM MuMuVMMHeadless.exe
-taskkill /F /IM MuMuVMMSVC.exe
-exit
